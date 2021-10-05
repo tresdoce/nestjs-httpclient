2de4a0388dc4e828464b1d2a4f31421d96466329
Mex Delgado <mdelgado@tresdoce.com.ar>
2021-10-05 19:34:35 -0300

    chore: first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,9 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
+  ignorePatterns:[
+    "**/__test__/**/*"
+  ],
   root: true,
   env: {
     node: true,
